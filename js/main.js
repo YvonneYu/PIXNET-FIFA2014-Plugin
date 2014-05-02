14d3508e5d4d4891346fa51ab3a5fba525509eba
YvonneYu <monkey6277@gmail.com>
2014-05-02 20:07:29 +0800

    add directive and controller

--- js/main.js
+++ js/main.js
@@ -2,5 +2,24 @@
  * Created by yue on 2014/5/2.
  */
 
-angular.module('App',
-        ['mainService']);
+angular.module('App', ['mainService'])
+        .controller('authorController', ['$scope', '$http', function($scope, $http) {
+                    $http.get('http://emma.pixnet.cc/users/skp?format=json', function(data) {
+                        $scope.author = {
+                                name: 'Naomi',
+                                address: '1600 Amphitheatre'
+                        };
+                });
+        }])
+        .directive('authorSection', function() {
+                function link(scope, element, attrs) {
+                        
+                }
+                
+                return {
+                        restrict: 'A',
+                        //templateUrl: 'my-section.html',
+                        link: link
+                        
+                };
+         });
